d621047e0368abb508e0a0dacc478db1fc20e46b
tfung <thomas.fung1@gmail.com>
2026-01-31 17:10:08 -0700

    remove welcome scaffolding

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,5 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    // index("routes/home.tsx"),
-    // route("mobile-app-editor", "routes/mobile-app-editor.tsx")
     index("routes/mobile-app-editor.tsx"),
-    route("home", "routes/home.tsx")
 ] satisfies RouteConfig;
