29e1847cd7240f1ea0ebdaf90add032028d12512
tfung <thomas.fung1@gmail.com>
2026-02-01 02:25:10 -0700

    add port customization for app

--- mobile-app-editor-app/vite.config.ts
+++ mobile-app-editor-app/vite.config.ts
@@ -1,8 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      port: parseInt(env.PORT || "5173"),
+    },
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  };
 });
